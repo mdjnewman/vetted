434609b6b247cd2145d2f57630f4cec7304e7f06
Matt Newman <mdjnewman@gmail.com>
2018-01-29 19:24:07 -0500

    WIP - initial attempt to use Feign with CompletableFuture

--- api/importer/src/main/kotlin/me/mdjnewman/vetted/importer/Importer.kt
+++ api/importer/src/main/kotlin/me/mdjnewman/vetted/importer/Importer.kt
@@ -1,16 +1,20 @@
 package me.mdjnewman.vetted.importer
 
 import com.healthmarketscience.jackcess.DatabaseBuilder
-import com.healthmarketscience.jackcess.util.ExportUtil
+import com.healthmarketscience.jackcess.Row
+import com.healthmarketscience.jackcess.Table
 import me.mdjnewman.vetted.client.VettedClientResource
-import me.mdjnewman.vetted.model.AddClientNoteCommandDTO
+import me.mdjnewman.vetted.model.AddressDTO
+import me.mdjnewman.vetted.model.CreateClientCommandDTO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.feign.EnableFeignClients
 import org.springframework.context.annotation.Bean
 import java.io.File
 import java.util.UUID
+import java.util.concurrent.CompletableFuture
 
 @SpringBootApplication
 @EnableFeignClients(basePackageClasses = arrayOf(VettedClientResource::class))
@@ -21,19 +25,48 @@ class Application {
         clientResource: VettedClientResource,
         @Value("\${vetted-web.ribbon.listOfServers}") servers: List<String>
     ) = CommandLineRunner {
-        print(servers)
-        clientResource.addNote(AddClientNoteCommandDTO(UUID.randomUUID(), "asd"))
-        print(clientResource.toString())
+        val database = DatabaseBuilder.open(File("/Users/mnewman/projects/mdbtools/main.accdb"))
+
+//        database.tableNames.forEach { println(it) }
+//        database.queries.forEach { println(it.name) }
+//        ExportUtil.exportAll(database, File("/Users/mnewman/Desktop/foo"), "csv", true)
+
+        val futures = database.getTable("Clients")
+            .map {
+                CreateClientCommandDTO(
+                    clientId = UUID.randomUUID(),
+                    name = getConcatenatedName(it["First Name"] as String?, it["Last Name/Trading Name"] as String?),
+                    address = AddressDTO(
+                        addressLineOne = it["Street Address/PO Box"] as String,
+                        addressLineTwo = null,
+                        postcode = getPostcode(it["Town"] as String, database.getTable("Postcodes")),
+                        town = it["Town"] as String,
+                        state = getState(it["Town"] as String, database.getTable("Postcodes"))
+                    )
+                )
+            }
+            .map { c -> clientResource.create(c) }
+            .toTypedArray()
+
+        CompletableFuture.allOf(*futures)
     }
 }
 
 fun main(args: Array<String>) {
-    val database = DatabaseBuilder.open(File("/Users/mnewman/projects/mdbtools/main.accdb"))
-    database.tableNames.forEach { println(it) }
-    database.queries.forEach { println(it.name) }
-
-    ExportUtil.exportAll(database, File("/Users/mnewman/Desktop/foo"), ".csv", true)
-
-    val table = database.getTable("Transactions")
-    table.last().let { println("Column 'TransactionID' has value: " + it["TransactionID"]) }
+    SpringApplication.run(Application::class.java, *args)
 }
+
+private fun getConcatenatedName(fname: String?, lname: String?): String =
+    listOf(fname, lname)
+        .filter { it != null && it.trim().isNotEmpty() }
+        .joinToString(" ")
+
+private fun getPostcode(town: String, table: Table): String =
+    findRowForTown(table, town)["Postcode"] as String
+
+private fun getState(town: String, table: Table): String =
+    findRowForTown(table, town)["State"] as String
+
+private fun findRowForTown(table: Table, town: String): Row =
+    table.firstOrNull { town.equals(it["Town"] as String, true) }
+        ?: throw RuntimeException("No row for town: $town")
