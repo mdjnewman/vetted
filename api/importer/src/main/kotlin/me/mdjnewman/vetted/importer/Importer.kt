98bb9c21c8b9d5e039d97423678c2601dc36c98d
Matt Newman <mdjnewman@gmail.com>
2018-01-31 18:34:19 -0500

    Add client notes

--- api/importer/src/main/kotlin/me/mdjnewman/vetted/importer/Importer.kt
+++ api/importer/src/main/kotlin/me/mdjnewman/vetted/importer/Importer.kt
@@ -4,11 +4,10 @@ import com.healthmarketscience.jackcess.DatabaseBuilder
 import com.healthmarketscience.jackcess.Row
 import com.healthmarketscience.jackcess.Table
 import me.mdjnewman.vetted.Address
+import me.mdjnewman.vetted.command.AddClientNoteCommand
 import me.mdjnewman.vetted.command.MigrateClientCommand
 import me.mdjnewman.vetted.core.VettedCoreMarker
 import org.axonframework.commandhandling.gateway.CommandGateway
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -17,48 +16,68 @@ import org.springframework.context.annotation.ComponentScan
 import java.io.File
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletableFuture.allOf
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = arrayOf(VettedCoreMarker::class))
 class Application {
 
-    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
-
     @Bean
     fun init(
         commandGateway: CommandGateway
-    ) = CommandLineRunner {
+    ) = CommandLineRunner { _ ->
         val databaseToImport = DatabaseBuilder.open(File("/Users/mnewman/projects/mdbtools/main.accdb"))
-        val postcodeTable = databaseToImport.getTable("Postcodes")
-        val clientsTable = databaseToImport.getTable("Clients")
 
-        val migrateCommandResults =
-            clientsTable
-                .map(::buildClientTableRow)
-                .map {
-                    MigrateClientCommand(
-                        clientId = UUID.randomUUID(),
-                        name = it.name,
-                        address = Address(
-                            addressLineOne = it.street,
-                            addressLineTwo = null,
-                            postcode = getPostcode(it.town, postcodeTable),
-                            town = it.town,
-                            state = getState(it.town, postcodeTable)
-                        ),
-                        priorId = it.clientId
-                    )
-                }
-                .map { commandGateway.send<Void>(it) }
-                .toTypedArray()
+        val clientTableRows = databaseToImport.getTable("Clients").map(::buildClientTableRow)
 
-        CompletableFuture.allOf(*migrateCommandResults).get()
+        val newClientIds: Map<String, UUID> = createClients(
+            clientTableRows,
+            databaseToImport.getTable("Postcodes"),
+            commandGateway
+        )
+
+        addClientNotes(clientTableRows, newClientIds, commandGateway)
     }
 }
 
-fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+private fun createClients(
+    clientTableRows: List<ClientTableRow>,
+    postcodeTable: Table,
+    commandGateway: CommandGateway
+): Map<String, UUID> {
+    val createClientCommands = clientTableRows.map { migrateClientCommand(it, postcodeTable) }
+
+    val newClientIds: Map<String, UUID> = createClientCommands.associate { it.priorId to it.clientId }
+
+    allOf(
+        *createClientCommands
+            .map { commandGateway.send<CompletableFuture<Void>>(it) }
+            .toTypedArray()
+    ).get()
+
+    return newClientIds
 }
+
+private fun addClientNotes(
+    clientTableRows: List<ClientTableRow>,
+    newClientIds: Map<String, UUID>,
+    commandGateway: CommandGateway
+) =
+    allOf(*clientTableRows
+        .filter { it.clientNotes != null }
+        .map {
+            AddClientNoteCommand(
+                clientId = newClientIds[it.clientId]!!,
+                noteText = it.clientNotes!!
+            )
+        }
+        .map { commandGateway.send<CompletableFuture<Void>>(it) }
+        .toTypedArray()
+    ).get()
 
 private fun getConcatenatedName(fName: String?, lName: String?): String =
     listOf(fName, lName)
@@ -75,22 +94,40 @@ private fun findRowForTown(table: Table, town: String): Row =
     table.firstOrNull { town.equals(it["Town"] as String, true) }
         ?: throw RuntimeException("No row for town: $town")
 
-private fun buildClientTableRow(row: Row): ClientTableRow {
-    return ClientTableRow(
+private fun buildClientTableRow(row: Row) =
+    ClientTableRow(
         fName = row["First Name"] as String?,
         lName = row["Last Name/Trading Name"] as String?,
         street = row["Street Address/PO Box"] as String,
         town = row["Town"] as String,
-        clientId = row["ClientId"] as String
+        clientId = (row["ClientID"] as Int).toString(),
+        clientNotes = (row["Client Notes"] as String?)?.trim()
     )
-}
 
 data class ClientTableRow(
     private val fName: String?,
     private val lName: String?,
     val street: String,
     val town: String,
-    val clientId: String
+    val clientId: String,
+    val clientNotes: String?
 ) {
     val name = getConcatenatedName(fName, lName)
 }
+
+private fun migrateClientCommand(
+    clientRow: ClientTableRow,
+    postcodeTable: Table
+) =
+    MigrateClientCommand(
+        clientId = UUID.randomUUID(),
+        name = clientRow.name,
+        address = Address(
+            addressLineOne = clientRow.street,
+            addressLineTwo = null,
+            postcode = getPostcode(clientRow.town, postcodeTable),
+            town = clientRow.town,
+            state = getState(clientRow.town, postcodeTable)
+        ),
+        priorId = clientRow.clientId
+    )
