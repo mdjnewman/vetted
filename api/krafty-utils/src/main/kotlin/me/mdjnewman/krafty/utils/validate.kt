a1422f5aa4f4b833efed5dd2dd52ecd19045a25e
Matt Newman <mdjnewman@gmail.com>
2018-01-31 19:50:30 -0500

    Avoid creating a validator all the damn time

--- api/krafty-utils/src/main/kotlin/me/mdjnewman/krafty/utils/validate.kt
+++ api/krafty-utils/src/main/kotlin/me/mdjnewman/krafty/utils/validate.kt
@@ -3,9 +3,9 @@ package me.mdjnewman.krafty.utils
 import javax.validation.ConstraintViolationException
 import javax.validation.Validation
 
+private val validator = Validation.buildDefaultValidatorFactory().validator
+
 fun Any.validate() {
-    val factory = Validation.buildDefaultValidatorFactory()
-    val validator = factory.validator
     val set = validator.validate(this)
     if (!set.isEmpty()) {
         throw ConstraintViolationException("Error creating ${this::class.simpleName}", set)
