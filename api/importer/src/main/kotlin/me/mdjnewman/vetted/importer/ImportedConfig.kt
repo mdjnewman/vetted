54da0c271ee447c78e4f74dcfb24cbeba5bcd556
Matt Newman <mdjnewman@gmail.com>
2018-03-11 18:05:58 -0500

    Split config to get tests running

---
+++ api/importer/src/main/kotlin/me/mdjnewman/vetted/importer/ImportedConfig.kt
@@ -0,0 +1,15 @@
+package me.mdjnewman.vetted.importer
+
+import me.mdjnewman.vetted.core.VettedCoreConfig
+import me.mdjnewman.vetted.coreinfrastructure.VettedCoreInfrastructureConfig
+import me.mdjnewman.vetted.query.VettedQueryConfig
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+
+@Configuration
+@Import(value = [
+    VettedCoreConfig::class,
+    VettedCoreInfrastructureConfig::class,
+    VettedQueryConfig::class
+])
+class ImportedConfig
--- api/web/src/main/kotlin/me/mdjnewman/vetted/web/Application.kt
+++ api/web/src/main/kotlin/me/mdjnewman/vetted/web/Application.kt
@@ -1,20 +1,12 @@
 package me.mdjnewman.vetted.web
 
-import me.mdjnewman.vetted.core.VettedCoreConfig
-import me.mdjnewman.vetted.coreinfrastructure.VettedCoreInfrastructureConfig
-import me.mdjnewman.vetted.query.VettedQueryConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(value = [
-    VettedCoreConfig::class,
-    VettedCoreInfrastructureConfig::class,
-    VettedQueryConfig::class
-])
 class Application
 
 fun main(args: Array<String>) {
-    SpringApplication.run(Application::class.java, *args)
+    SpringApplication
+        .run(Application::class.java, *args)
 }
