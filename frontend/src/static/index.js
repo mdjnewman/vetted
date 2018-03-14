57af92377fd515e198068cea83308c584f076440
Matt Newman <mdjnewman@gmail.com>
2018-03-14 19:11:34 -0400

    reset --hard frontend/ - trying React

--- frontend/src/static/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require("bootstrap");
-
-require('bootstrap/dist/css/bootstrap.css');
-
-// inject bundled Elm app into div#main
-var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
